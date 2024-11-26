d36545010a4de426848e75ef16d0a9dc8e112477
JakeGrey-ctrl <kirill-904006@mail.ru>
2024-11-26 22:00:27 +0300

    Update zadanie 1.py

--- Лабораторная №7/zadanie 1.py
+++ Лабораторная №7/zadanie 1.py
@@ -18,8 +18,8 @@ rows_count = input("Введите количество столбцов: ")
 input_invalid = True
 while input_invalid:
     try:
-        columns_count = int(columns_count)
-        if columns_count >= 1:
+        rows_count = int(rows_count)
+        if rows_count >= 1:
             input_invalid = False
         else:
             print("Вы должны ввести целое положительное число!")
@@ -27,6 +27,7 @@ while input_invalid:
     except ValueError:
         print("Вы должны ввести целое положительное число!")
         rows_count = input("Попробуйте ввести количество столбцов ещё раз: ")
+
 matrix = [[randint(-10, 10) for i in range(rows_count)] for j in range(columns_count)] ##версия с генератором
 ##Конец проверки ввода пользователя
 ##matrix = []
@@ -35,7 +36,14 @@ matrix = [[randint(-10, 10) for i in range(rows_count)] for j in range(columns_c
 ##    for j in range(rows_count):
 ##        row.append(randint(-10, 10))
 ##    matrix.append(row)
+
 ##Вывод входного массива (начало)
 print("Входной массив: ")
 for row in matrix:
-    print(*row)
+    for i in row:
+        if 0 <= i <= 9:
+            print(" " + str(i), end = " ") ##сделано в целях грамотного отображения
+        else:
+            print(i, end = " ")
+    print()
+##Конец вывода входного массива
