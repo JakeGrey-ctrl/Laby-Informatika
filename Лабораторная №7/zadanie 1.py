6ba8e41fe906819fe44f34d03bf7f3d43f69a44f
JakeGrey-ctrl <kirill-904006@mail.ru>
2024-11-26 21:46:16 +0300

    Create zadanie 1.py

---
+++ Лабораторная №7/zadanie 1.py
@@ -0,0 +1,41 @@
+from random import randint
+columns_count = input("Введите количество строк: ")
+##Начало проверки ввода пользователя
+input_invalid = True
+while input_invalid:
+    try:
+        columns_count = int(columns_count)
+        if columns_count >= 1:
+            input_invalid = False
+        else:
+            print("Вы должны ввести целое положительное число!")
+            columns_count = input("Попробуйте ввести количество строк ещё раз: ")
+    except ValueError:
+        print("Вы должны ввести целое положительное число!")
+        columns_count = input("Попробуйте ввести количество строк ещё раз: ")
+
+rows_count = input("Введите количество столбцов: ")
+input_invalid = True
+while input_invalid:
+    try:
+        columns_count = int(columns_count)
+        if columns_count >= 1:
+            input_invalid = False
+        else:
+            print("Вы должны ввести целое положительное число!")
+            rows_count = input("Попробуйте ввести количество столбцов ещё раз: ")
+    except ValueError:
+        print("Вы должны ввести целое положительное число!")
+        rows_count = input("Попробуйте ввести количество столбцов ещё раз: ")
+matrix = [[randint(-10, 10) for i in range(rows_count)] for j in range(columns_count)] ##версия с генератором
+##Конец проверки ввода пользователя
+##matrix = []
+##for i in range(columns_count): ##версия без использования генератора
+##    row = []
+##    for j in range(rows_count):
+##        row.append(randint(-10, 10))
+##    matrix.append(row)
+##Вывод входного массива (начало)
+print("Входной массив: ")
+for row in matrix:
+    print(*row)
