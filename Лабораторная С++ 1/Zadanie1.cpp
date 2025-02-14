a1309ffe9407f06235d80a6293f2c017ac8d8178
JakeGrey-ctrl <kirill-904006@mail.ru>
2025-02-14 08:41:21 +0300

    var30

--- Лабораторная С++ 1/Zadanie1.cpp
+++ Лабораторная С++ 1/Zadanie1.cpp
@@ -1,5 +1,5 @@
 ﻿#include <iostream>
-#include <string>
+#include <limits>
 
 using namespace std;
 
@@ -7,36 +7,25 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	double x, y, z;
-	string Sx, Sy;
 	bool incorrect_input = true;
 	cout << "Введите x: ";
-	while (incorrect_input) //проверка ввода пользователя
+	cin >> x;
+	while (cin.fail()) //проверка ввода пользователя
 	{
-		try
-		{
-			cin >> Sx;
-			x = stod(Sx);
-			incorrect_input = false;
-		}
-		catch (invalid_argument)
-		{
-			cout << "Неверный ввод! Попробуйте ещё раз ввести x: ";
-		}
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Некорректный ввод! Попробуйте ввести x ещё раз: ";
+		cin >> x;
 	}
 	incorrect_input = true;
 	cout << "Введите y: ";
-	while (incorrect_input) //проверка ввода пользователя
+	cin >> y;
+	while (cin.fail())
 	{
-		try
-		{
-			cin >> Sy;
-			y = stod(Sy);
-			incorrect_input = false;
-		}
-		catch (invalid_argument)
-		{
-			cout << "Неверный ввод! Попробуйте ещё раз ввести y: ";
-		}
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Некорректный ввод! Попробуйте ввести y ещё раз: ";
+		cin >> y;
 	}
 	z = sqrt(abs(pow(x, y) - cos(y)));
 	cout << "Значение выражения: " << z;
--- Лабораторная С++ 1/Zadanie2.cpp
+++ Лабораторная С++ 1/Zadanie2.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
-#include <string>
+#include <limits>
 
 using namespace std;
 
 int main1()
 {
 	setlocale(LC_ALL, "Russian");
-	string Sx;
 	double x, y;
 	bool incorrect_input = true;
 	cout << "Введите x: ";
-	while (incorrect_input) //проверка ввода пользователя
+	cin >> x;
+	while (cin.fail()) //проверка ввода пользователя
 	{
-		try
-		{
-			cin >> Sx;
-			x = stod(Sx);
-			incorrect_input = false;
-		}
-		catch (invalid_argument)
-		{
-			cout << "Неверный ввод! Попробуйте ещё раз ввести x: ";
-		}
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Некорректный ввод! Попробуйте ввести x ещё раз: ";
+		cin >> x;
 	}
 	if (x < -2.5) y = pow(sin(5 * pow(x, 2) + 3 * x + 1), 5);
 	else if (x < 4) y = x * sin(4 * x - 1);
